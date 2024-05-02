b5090e1e1b3fff84230f08e7349c880e2a065cf7
iqbal <iqbalmahbub420@gmail.com>
2024-05-02 18:45:36 +0600

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
+
 //import 'dart:ui';
-void main(){
+void main() {
   runApp(myApp());
 }
-class myApp extends StatelessWidget{
+
+class myApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Todo app',
+      title: 'Todo app',
       home: homeScreen(),
     );
   }
 }
+
 class homeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-
-          child: Text('Hello iqbal mahbub,whats your favorite colour .',
-            textAlign: TextAlign.center,
-            maxLines: 3,
-            style: TextStyle(
-            color: Colors.white,
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            //backgroundColor: Colors.purpleAccent,
-            decoration: TextDecoration.underline,
-            wordSpacing: 4,
-            letterSpacing: 2,
-              overflow: TextOverflow.ellipsis,
-
-          ),)),
-    );
+        appBar: AppBar(
+          leading: Icon(color: Colors.yellow, Icons.home),
+          title: Text('Home',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 32,
+              )),
+        ),
+        body: Column(
+            mainAxisAligment:
+            children: [
+          Text('Iqbal '),
+          Text('Mahbub'),
+          Text('I love you'),
+          Image.asset('img/iqbal.jpg'),
+        ]));
   }
 }
