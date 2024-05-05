fff4f34cf1566f2c2ca761565f65dec6234a2fe0
iqbal <iqbalmahbub420@gmail.com>
2024-05-05 23:00:08 +0600

    module 7 - border anr textField

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
+import 'package:flutter/material.dart';
 import 'dart:ui';
 
 void main() {
@@ -20,9 +21,84 @@ class homeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-    );
+        appBar: AppBar(
+          leading: Icon(color: Colors.red, Icons.home),
+          title: Text('Home'),
+          backgroundColor: Colors.pink,
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: Icon(
+            Icons.add,
+            size: 30,
+            opticalSize: 40,
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print("Hello ");
+                },
+                child: Text(
+                  'Tap',
+                  style: TextStyle(fontSize: 32, color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent.shade700,
+                    foregroundColor: Colors.black,
+                    shadowColor: Colors.green,
+                    elevation: 10,
+                    //padding: EdgeInsets.all(20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(),
+                    )),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Text('TextButton'),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    textStyle: TextStyle(fontSize: 32),
+                  )),
+              //IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.add,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  enabled: true,
+                  hintText: 'Enter your Name',
+                  label: Text('Name'),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.amber,
+                    width: 10,
+                    style: BorderStyle.solid,
+                  )),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 10,
+                    style: BorderStyle.solid,
+                  )),
+                  disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.yellowAccent.shade400,
+                    width: 10,
+                    style: BorderStyle.solid,
+                  )),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
