8b23a9b643d700b22ffb2417a9196531234c78fe
iqbal <iqbalmahbub420@gmail.com>
2024-05-09 00:18:07 +0600

    gridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,72 @@
-import 'dart:ffi';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'dart:ui';
 
-void main() {
-  runApp(myApp());
+main() {
+  runApp(App());
 }
 
-class myApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo app',
-      home: homeScreen(),
+      title: "Product Show App",
+      home: HomeActivity(),
     );
   }
 }
 
-class homeScreen extends StatelessWidget {
-  TextEditingController searchTextFileController = TextEditingController();
+var myItems = [
+  {"name": "Iqbal", "Phn": "01521574368", "types": "Friend"},
+  {"name": "John", "Phn": "0123456789", "types": "Family"},
+  {"name": "Alice", "Phn": "9876543210", "types": "Colleague"},
+  {"name": "Emily", "Phn": "1234567890", "types": "Neighbor"},
+  {"name": "Michael", "Phn": "5551234567", "types": "Classmate"},
+  {"name": "Sarah", "Phn": "4449876543", "types": "Co-worker"},
+  {"name": "David", "Phn": "3338765432", "types": "Acquaintance"},
+  {"name": "Jessica", "Phn": "7776543210", "types": "Friend"},
+  {"name": "Daniel", "Phn": "9998887776", "types": "Family"},
+  {"name": "Sophia", "Phn": "1112223334", "types": "Colleague"},
+  {"name": "James", "Phn": "6665554443", "types": "Neighbor"},
+  {"name": "Emma", "Phn": "8889990001", "types": "Classmate"},
+  {"name": "Ryan", "Phn": "5551239876", "types": "Co-worker"},
+  {"name": "Olivia", "Phn": "4449871234", "types": "Acquaintance"},
+  {"name": "Benjamin", "Phn": "3338769876", "types": "Friend"},
+  {"name": "Mia", "Phn": "7776540123", "types": "Family"},
+  {"name": "William", "Phn": "9998885678", "types": "Colleague"},
+  {"name": "Ella", "Phn": "1112228765", "types": "Neighbor"},
+  {"name": "Aiden", "Phn": "6665556543", "types": "Classmate"},
+  {"name": "Sofia", "Phn": "8889992345", "types": "Co-worker"}
+];
 
+mysSnackBar(context, msg) {
+  return ScaffoldMessenger.of(context)
+      .showSnackBar(SnackBar(content: Text(msg)));
+}
+
+class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Icon(color: Colors.red, Icons.home),
-          title: Text('Home'),
-          backgroundColor: Colors.pink,
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {},
-          label: Icon(
-            Icons.add,
-            size: 30,
-            opticalSize: 40,
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  print("Hello ");
-                },
-                child: Text(
-                  'Tap',
-                  style: TextStyle(fontSize: 32, color: Colors.black),
-                ),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent.shade700,
-                    foregroundColor: Colors.black,
-                    shadowColor: Colors.green,
-                    elevation: 10,
-                    //padding: EdgeInsets.all(20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(),
-                    )),
-              ),
-              TextButton(
-                  onPressed: () {},
-                  child: Text('TextButton'),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    textStyle: TextStyle(fontSize: 32),
-                  )),
-              //IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-              FloatingActionButton(
-                onPressed: () {},
-                child: Icon(
-                  Icons.add,
-                ),
-              ),
-              FloatingActionButton(
-                onPressed: () {},
-                child: Icon(
-                  Icons.add,
-                ),
-              ),
-              TextField(
-                controller: searchTextFileController,
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        backgroundColor: Colors.green,
+        title: Text('Home'),
+      ),
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, mainAxisSpacing: 2),
+          itemCount: myItems.length,
+          itemBuilder: (context, index) {
+            return InkWell(
                 onTap: () {
-                  print('Taped');
+                  mysSnackBar(context, myItems[index]['types']);
                 },
-                keyboardType: TextInputType.number,
-                onChanged: (String input) {
-                  print(input);
-                },
-                textInputAction: TextInputAction.done,
-                onSubmitted: (String value) {
-                  print(searchTextFileController.text);
-                  searchTextFileController.clear();
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  enabled: true,
-                  hintText: 'Enter your Name',
-                  label: Text('Name'),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                    color: Colors.amber,
-                    width: 10,
-                    style: BorderStyle.solid,
-                  )),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                    color: Colors.red,
-                    width: 10,
-                    style: BorderStyle.solid,
-                  )),
-                  disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                    color: Colors.yellowAccent.shade400,
-                    width: 10,
-                    style: BorderStyle.solid,
-                  )),
-                ),
-              ),
-            ],
-          ),
-        ));
+                child: ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text(myItems[index]['name']!),
+                  subtitle: Text(myItems[index]['Phn']!),
+                ));
+          }),
+    );
   }
 }
