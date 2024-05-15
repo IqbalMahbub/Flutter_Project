8532b8ee125e49d3b13ffa5747dffdcbe760704c
iqbal <iqbalmahbub420@gmail.com>
2024-05-15 17:31:22 +0600

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
+
 void main(){
-  runApp(myApp());
+  runApp(TodoApp());
 }
-class myApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:'Todo app',
-      home: homeScreen(),
-    );
-  }
-}
-class homeScreen extends StatelessWidget {
+
+class TodoApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightGreen,
-      body: Center(child: Text('Hello World8')),
+    return MaterialApp(
+      title: 'Todolist App',
     );
   }
+
 }
