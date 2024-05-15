4c55f5bedb45b6db09bb7360c6ee780491adadee
iqbal <iqbalmahbub420@gmail.com>
2024-05-15 17:40:12 +0600

    basic app

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,31 @@ class TodoApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todolist App',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: HomeScreen(),
     );
   }
 
 }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Text('TODO LIST'),
+      ),
+    );
+  }
+}
+
