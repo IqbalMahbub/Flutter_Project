a98c15b169a1123aec621b3fdb94791e173b0cfb
iqbal <iqbalmahbub420@gmail.com>
2024-05-15 12:21:49 +0600

    todo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,126 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(App());
+void main() {
+  runApp(TodoListApp());
 }
 
-class App extends StatelessWidget {
+class TodoListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      title: 'Todo List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
+class TodoListScreen extends StatefulWidget {
   @override
-  State<HomeScreen> createState() => _HomeScreen();
+  _TodoListScreenState createState() => _TodoListScreenState();
 }
 
-class _HomeScreen extends State<HomeScreen> {
-  int count = 0;
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<TodoItem> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _todoItems.add(TodoItem(task: task, isDone: false));
+      });
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() {
+      _todoItems.removeAt(index);
+    });
+  }
+
+  void _toggleTodoItem(int index) {
+    setState(() {
+      _todoItems[index].isDone = !_todoItems[index].isDone;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.pink.shade100,
         leading: Icon(Icons.home),
         title: Text('Home'),
       ),
-      body: Center(
-        child: Text(count.toString(),
-            style: TextStyle(color: Colors.blue, fontSize: 35)),
+      body: ListView.builder(
+        itemCount: _todoItems.length,
+        itemBuilder: (context, index) {
+          return CheckboxListTile(
+            title: Text(_todoItems[index].task),
+            subtitle: ElevatedButton(
+              onPressed: () {
+                _removeTodoItem(index);
+              },
+              child: Icon(Icons.delete),
+            ),
+            value: _todoItems[index].isDone,
+            onChanged: (bool? isChecked) {
+              _toggleTodoItem(index);
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          count++;
-          setState(() {});
+          _promptAddTodoItem(context);
         },
+        tooltip: 'Add task',
         child: Icon(Icons.add),
       ),
     );
   }
+
+  void _promptAddTodoItem(BuildContext context) {
+    String task = '';
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add a new task'),
+          content: TextField(
+            autofocus: true,
+            onChanged: (value) {
+              task = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _addTodoItem(task);
+                Navigator.of(context).pop();
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class TodoItem {
+  String task;
+  bool isDone;
+
+  TodoItem({required this.task, required this.isDone});
 }
