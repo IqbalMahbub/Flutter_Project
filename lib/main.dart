ffab37a3e61a07b783b90b7b1b9e611f1acac262
iqbal <iqbalmahbub420@gmail.com>
2024-05-21 00:21:32 +0600

    SizedBox,AspectRatio

--- lib/main.dart
+++ lib/main.dart
@@ -26,54 +26,63 @@ class _HomeActivityState extends State<HomeActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.home),
-        title: Text('Home'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.blueAccent,
-              child: Text("hfjxvj "
-                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+        appBar: AppBar(
+          leading: const Icon(Icons.home),
+          title: const Text('Home'),
+        ),
+        // body: Column(
+        //   children: [
+        //     Expanded(
+        //       flex: 3,
+        //       child: Container(
+        //         height: 100,
+        //         width: double.infinity,
+        //         color: Colors.blueAccent,
+        //         child: Text("hfjxvj "
+        //             "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+        //       ),
+        //     ),
+        //     Expanded(
+        //       flex: 2,
+        //       child: Container(
+        //         height: 100,
+        //         width: double.infinity,
+        //         color: Colors.pink,
+        //         child: Text("hfjxvj "
+        //             "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+        //       ),
+        //     ),
+        //     Expanded(
+        //       flex: 5,
+        //       child: Container(
+        //         height: 100,
+        //         width: double.infinity,
+        //         color: Colors.green,
+        //         child: Text("hfjxvj "
+        //             "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+        //       ),
+        //     ),
+        //     Expanded(
+        //       flex: 3,
+        //       child: SizedBox(
+        //         height: 100,
+        //         width: double.infinity,
+        //         child: Text("hfjxvj "
+        //             "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+        //       ),
+        //     ),
+        body: SizedBox(
+          width: 100,
+          child: AspectRatio(
+            aspectRatio: 2 / 5,
+            child: FractionallySizedBox(
+              heightFactor: 100,
+              widthFactor: 10,
+              alignment: Alignment.topLeft,
+              child: Text(
+                  "fghjkl;dsfdgfhgjhkjlk fgfhgjhgfdsetyhjkghgfgdfhjhsfhfjfh"),
             ),
           ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.pink,
-              child: Text("hfjxvj "
-                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
-            ),
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.green,
-              child: Text("hfjxvj "
-                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.yellow,
-              child: Text("hfjxvj "
-                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
