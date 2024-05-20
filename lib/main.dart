47aba7dc4b588a85c73f065f759622ae7008e7be
iqbal <iqbalmahbub420@gmail.com>
2024-05-20 23:55:51 +0600

    flexible

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,83 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-main() {
-  runApp(App());
+void main() {
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Product Show App",
+    return const MaterialApp(
       home: HomeActivity(),
     );
   }
 }
 
-var myItems = [
-  {"name": "Iqbal", "Phn": "01521574368", "types": "Friend"},
-  {"name": "John", "Phn": "0123456789", "types": "Family"},
-  {"name": "Alice", "Phn": "9876543210", "types": "Colleague"},
-  {"name": "Emily", "Phn": "1234567890", "types": "Neighbor"},
-  {"name": "Michael", "Phn": "5551234567", "types": "Classmate"},
-  {"name": "Sarah", "Phn": "4449876543", "types": "Co-worker"},
-  {"name": "David", "Phn": "3338765432", "types": "Acquaintance"},
-  {"name": "Jessica", "Phn": "7776543210", "types": "Friend"},
-  {"name": "Daniel", "Phn": "9998887776", "types": "Family"},
-  {"name": "Sophia", "Phn": "1112223334", "types": "Colleague"},
-  {"name": "James", "Phn": "6665554443", "types": "Neighbor"},
-  {"name": "Emma", "Phn": "8889990001", "types": "Classmate"},
-  {"name": "Ryan", "Phn": "5551239876", "types": "Co-worker"},
-  {"name": "Olivia", "Phn": "4449871234", "types": "Acquaintance"},
-  {"name": "Benjamin", "Phn": "3338769876", "types": "Friend"},
-  {"name": "Mia", "Phn": "7776540123", "types": "Family"},
-  {"name": "William", "Phn": "9998885678", "types": "Colleague"},
-  {"name": "Ella", "Phn": "1112228765", "types": "Neighbor"},
-  {"name": "Aiden", "Phn": "6665556543", "types": "Classmate"},
-  {"name": "Sofia", "Phn": "8889992345", "types": "Co-worker"}
-];
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
 
-mysSnackBar(context, msg) {
-  return ScaffoldMessenger.of(context)
-      .showSnackBar(SnackBar(content: Text(msg)));
+  @override
+  State<HomeActivity> createState() => _HomeActivityState();
 }
 
-class HomeActivity extends StatelessWidget {
+class _HomeActivityState extends State<HomeActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.home),
-        backgroundColor: Colors.green,
+        leading: const Icon(Icons.home),
         title: Text('Home'),
       ),
-      body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, mainAxisSpacing: 2),
-          itemCount: myItems.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-                onTap: () {
-                  mysSnackBar(context, myItems[index]['types']);
-                },
-                child: ListTile(
-                  leading: Icon(Icons.access_alarm),
-                  title: Text(myItems[index]['name']!),
-                  subtitle: Text(myItems[index]['Phn']!),
-                ));
-          }),
+      body: Column(
+        children: [
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 3,
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.blueAccent,
+              child: Text("hfjxvj "
+                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 2,
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.pink,
+              child: Text("hfjxvj "
+                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 5,
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.green,
+              child: Text("hfjxvj "
+                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 3,
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.yellow,
+              child: Text("hfjxvj "
+                  "kvkjdvdvdvdvdvdsfdfkhjgfddgkjkhjgfdsdfdhjhjkl;kljkhjghfdfssa"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
