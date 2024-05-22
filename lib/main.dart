6ac24be84040aee3fdf342f2cc27b21b1f7d839d
iqbal <iqbalmahbub420@gmail.com>
2024-05-22 20:40:14 +0600

    navebar with Sccafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_project/profileScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -26,19 +27,57 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   List<Widget> screen = [
-    Text('Home'),
-    Text('Prfile'),
-    Text('Search'),
-    Text('Setting'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.home),
         title: Text('Home'),
       ),
+      body: Center(child: const Text('Home')),
+    ),
+    Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.person),
+        title: Text('Profile'),
+      ),
+      body: Center(child: const Text('Profile')),
+    ),
+    Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.search),
+        title: Text('Search'),
+      ),
+      body: Center(child: const Text('Search')),
+    ),
+    Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.settings),
+        title: Text('settings'),
+      ),
+      body: Center(child: const Text('settings')),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    if (index == 1) {
+      // Navigate to the profile screen
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => profileScreen()),
+      );
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   leading: Icon(Icons.home),
+      //   title: Text('Home'),
+      // ),
       body: Center(
         child: screen[_selectedIndex],
       ),
---
+++ lib/profileScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class profileScreen extends StatefulWidget {
+  const profileScreen({super.key});
+
+  @override
+  State<profileScreen> createState() => _profileScreenState();
+}
+
+class _profileScreenState extends State<profileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.person),
+        title: const Text('Profile'),
+      ),
+    );
+  }
+}
