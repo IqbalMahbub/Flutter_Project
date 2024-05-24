e48d5d9b3ecd9337219d622d3ab3f7b1f55f5acf
iqbal <iqbalmahbub420@gmail.com>
2024-05-24 08:25:12 +0600

    navebar with Sccafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_flutter_project/profileScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(Myapp());
@@ -12,7 +13,7 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       title: 'MyApp',
       theme: ThemeData(colorSchemeSeed: Colors.blueAccent),
-      home: const HomeScreen(),
+      home: const SecondScreen(),
     );
   }
 }
@@ -29,31 +30,31 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Widget> screen = [
     Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.home),
-        title: Text('Home'),
+        leading: const Icon(Icons.home),
+        title: const Text('Home'),
       ),
-      body: Center(child: const Text('Home')),
+      body: const Center(child: Text('Home')),
     ),
     Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.person),
-        title: Text('Profile'),
+        leading: const Icon(Icons.person),
+        title: const Text('Profile'),
       ),
-      body: Center(child: const Text('Profile')),
+      body: const Center(child: Text('Profile')),
     ),
     Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.search),
-        title: Text('Search'),
+        leading: const Icon(Icons.search),
+        title: const Text('Search'),
       ),
-      body: Center(child: const Text('Search')),
+      body: const Center(child: Text('Search')),
     ),
     Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.settings),
-        title: Text('settings'),
+        leading: const Icon(Icons.settings),
+        title: const Text('settings'),
       ),
-      body: Center(child: const Text('settings')),
+      body: const Center(child: Text('settings')),
     ),
   ];
 
@@ -62,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
       // Navigate to the profile screen
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => profileScreen()),
+        MaterialPageRoute(builder: (context) => const profileScreen()),
       );
     } else {
       setState(() {
@@ -85,7 +86,7 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blueAccent,
         selectedItemColor: Colors.green,
         unselectedItemColor: Colors.grey,
-        selectedLabelStyle: TextStyle(),
+        selectedLabelStyle: const TextStyle(),
         showUnselectedLabels: true,
         currentIndex: _selectedIndex,
         onTap: (index) {
@@ -99,6 +100,96 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
         ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: DefaultTabController(
+        length: 6,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            // leading: const Icon(Icons.access_alarm),
+            title: const Text(
+              'Facebook',
+              style: TextStyle(color: Colors.lightBlue),
+            ),
+
+            actions: const [
+              Icon(Icons.add),
+              Icon(Icons.search),
+              Icon(Icons.menu)
+            ],
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home_filled),
+                ),
+                Tab(
+                  icon: Icon(Icons.people),
+                ),
+                Tab(
+                  icon: Icon(Icons.send),
+                ),
+                Tab(
+                  icon: Icon(Icons.notifications),
+                ),
+                Tab(
+                  icon: Icon(Icons.video_collection),
+                ),
+                Tab(
+                  icon: Icon(Icons.shop),
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              SizedBox(
+                child: Center(
+                  child: Text('Home'),
+                ),
+              ),
+              SizedBox(
+                child: Center(
+                  child: Text('Friends'),
+                ),
+              ),
+              SizedBox(
+                child: Center(
+                  child: Text('Massege'),
+                ),
+              ),
+              SizedBox(
+                  child: Center(
+                child: Text('Notification'),
+              )),
+              SizedBox(
+                child: Center(
+                  child: Text('Vedios'),
+                ),
+              ),
+              SizedBox(
+                child: Center(
+                  child: Text('MarketPlace'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
